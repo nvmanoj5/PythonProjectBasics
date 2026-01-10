4450bf87df11d31c1c987ad7e75503988d6b6aa2
Manoj <mailtosushmins@gmail.com>
2026-01-10 14:57:08 +0530

    Added for loop

---
+++ Forloop.py
@@ -0,0 +1,28 @@
+
+obj = [2, 3, 6, 8,9]
+
+for i in obj:
+    print(i)
+
+#Multiplication
+for i in obj:
+    print(i* 2)
+
+#Sum of first natural numbers
+for i in range(1, 6):
+    print(i)
+
+summation =0
+for j in range(1, 6):
+    summation = summation + j
+print(summation)
+
+print("*********")
+# Incrementing i value
+for i in range(1, 10, 2): # Every time it will add 2
+    print(i)
+
+print("*********")
+#If you don't give initial index it will start from 0
+for i in range(10):
+    print(i)
